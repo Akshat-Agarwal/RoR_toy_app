c4d1b3550adbb5516ca4a8f01f61086fbd23c725
Akshat Agarwal <akshat_agarwal@hotmail.com>
2016-02-02 03:38:03 +0000

    scaffolded a user database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,5 +55,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   
-  root 'application#hello'
+  #root 'application#hello'
+  resources :users
+  root 'users#index'
 end
